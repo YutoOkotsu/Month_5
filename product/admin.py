c9e038f6474909045b9b0373bf0d08bbfdccc30d
YutoOkotsu <karypbekov77@gmail.com>
2024-03-15 18:26:37 +0600

    homework1

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from product.models import Product, Category, Review
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Review)
--- product/views.py
+++ product/views.py
@@ -3,13 +3,16 @@ from rest_framework import viewsets
 from .models import Category, Product, Review
 from .serializers import CategorySerializer, ProductSerializer, ReviewSerializer
 
+
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
