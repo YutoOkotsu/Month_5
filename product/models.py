0c3c54ec3fb78c49225b184829ee00f1137949e8
YutoOkotsu <karypbekov77@gmail.com>
2024-03-19 23:52:35 +0600

    homework2

--- product/models.py
+++ product/models.py
@@ -15,5 +15,4 @@ class Product(models.Model):
 class Review(models.Model):
     text = models.TextField()
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-
-
+    stars = models.IntegerField(choices=[(i, i) for i in range(1, 6)], default=3)
--- product/serializers.py
+++ product/serializers.py
@@ -1,20 +1,37 @@
 from rest_framework import serializers
 from .models import Category, Product, Review
+from django.db.models import Avg
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    products_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Category
-        fields = ['id', 'name']
+        fields = ['id', 'name', 'products_count']
 
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ['id', 'title', 'description', 'price', 'category']
+    def get_products_count(self, obj):
+        return Product.objects.filter(category=obj).count()
 
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = ['id', 'text', 'product']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+    rating = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'price', 'category', 'reviews', 'rating']
+
+    def get_rating(self, obj):
+        reviews = Review.objects.filter(product=obj)
+        return reviews.aggregate(Avg('stars'))['stars__avg']
+
+
+
+
