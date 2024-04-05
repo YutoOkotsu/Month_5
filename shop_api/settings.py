e17592aa2bef9efc91c5e4fc95eb528f614a6185
YutoOkotsu <karypbekov77@gmail.com>
2024-04-05 14:39:28 +0600

    homework7

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -16,6 +16,14 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+import dotenv
+import os
+dotenv.load_dotenv('.env')
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -78,10 +86,15 @@ WSGI_APPLICATION = 'shop_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'your_database_name',
+        'USER': 'your_database_user',
+        'PASSWORD': 'your_database_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
